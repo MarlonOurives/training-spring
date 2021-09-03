be746151c58e3a44cef9ae6f358bd10b041eaf72
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-09-02 22:31:20 -0400

    correting membershipreposidoty

--- src/main/java/br/com/training/repository/MembershipRepository.java
+++ src/main/java/br/com/training/repository/MembershipRepository.java
@@ -1,11 +1,14 @@
 package br.com.training.repository;
 
+import br.com.training.model.Membership;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Repository
-public interface MembershipRepository extends JpaRepository<MembershipRepository, Long> {
-    Optional<MembershipRepository> findByName(String name);
+public interface MembershipRepository extends JpaRepository<Membership, Long> {
+    Optional<Membership> findByName(String name);
+
 }
