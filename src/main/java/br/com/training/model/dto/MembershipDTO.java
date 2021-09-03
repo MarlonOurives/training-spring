0d79afa503529815a1eaf9067778e1613c3fcbde
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-09-02 21:37:08 -0400

    add methods membershipDTO

--- src/main/java/br/com/training/model/dto/MembershipDTO.java
+++ src/main/java/br/com/training/model/dto/MembershipDTO.java
@@ -22,7 +22,62 @@ public class MembershipDTO {
     private String level;
 
     private String goal;
-    
+
     private String description;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBithDate() {
+        return bithDate;
+    }
+
+    public void setBithDate(LocalDate bithDate) {
+        this.bithDate = bithDate;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
