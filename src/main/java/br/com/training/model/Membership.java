d76758363fbfeaa2cc8a9c77898ef838ea539145
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-09-02 21:17:03 -0400

    model membership

---
+++ src/main/java/br/com/training/model/Membership.java
@@ -0,0 +1,90 @@
+package br.com.training.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_membership")
+public class Membership {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "bith_date")
+    private LocalDate bithDate;
+
+    @Column(name = "weight")
+    private Double weight;
+
+    @Column(name = "level")
+    private String level;
+
+    @Column(name = "goal")
+    private String goal;
+
+    @Column(name = "description")
+    private String description;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBithDate() {
+        return bithDate;
+    }
+
+    public void setBithDate(LocalDate bithDate) {
+        this.bithDate = bithDate;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
