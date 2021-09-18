203bc40d76abf8be9d9c1aeeedaee8492cdac2c9
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-09-18 16:58:11 -0400

    add findByName in MuscleRepository

--- src/main/java/br/com/training/repository/MuscleRepository.java
+++ src/main/java/br/com/training/repository/MuscleRepository.java
@@ -1,8 +1,13 @@
 package br.com.training.repository;
 
+import br.com.training.model.Muscle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MuscleRepository extends JpaRepository<MuscleRepository, Long> {
+    Optional<Muscle> findByName(String name);
+
 }
