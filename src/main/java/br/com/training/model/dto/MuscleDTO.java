91e0352ca20e0e6f7bdc10676318a7641b32d7e9
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-09-18 08:46:30 -0400

    dto muscle

---
+++ src/main/java/br/com/training/model/dto/MuscleDTO.java
@@ -0,0 +1,66 @@
+package br.com.training.model.dto;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+
+public class MuscleDTO {
+    private Long id;
+
+    @NotNull
+    private String name;
+    @NotNull
+    private String series;
+    @NotNull
+    private String repetition;
+    
+    private String time;
+    private String weekday;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getRepetition() {
+        return repetition;
+    }
+
+    public void setRepetition(String repetition) {
+        this.repetition = repetition;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getWeekday() {
+        return weekday;
+    }
+
+    public void setWeekday(String weekday) {
+        this.weekday = weekday;
+    }
+}
