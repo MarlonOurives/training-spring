4de17f87091b2438727abab201da69cf9943fff4
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-09-18 08:40:06 -0400

    renamed musclerepository

--- src/main/java/br/com/training/repository/MuscleRepository.java
+++ src/main/java/br/com/training/repository/MuscleRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Muscle extends JpaRepository<Muscle, Long> {
+public interface MuscleRepository extends JpaRepository<MuscleRepository, Long> {
 }
