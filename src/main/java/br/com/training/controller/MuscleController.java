5dd4c9bb3fdd75da7e05533a728d6aaf101e9e34
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-09-21 14:31:07 -0400

    MuscleController function save

---
+++ src/main/java/br/com/training/controller/MuscleController.java
@@ -0,0 +1,23 @@
+package br.com.training.controller;
+
+import br.com.training.model.dto.MuscleDTO;
+import br.com.training.service.MuscleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/muscle")
+public class MuscleController {
+
+    @Autowired
+    private MuscleService muscleService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MuscleDTO> save(@Valid @RequestBody MuscleDTO muscleDTO){
+        return ResponseEntity.ok(muscleService.save(muscleDTO));
+    }
+}
